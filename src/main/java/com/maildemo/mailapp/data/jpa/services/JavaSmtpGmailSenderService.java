a4f6a27b3a63263e3b84f3871e92c8fac06f54df
ssbhagwat212 <ssbhagwat212@gmail.com>
2025-05-04 10:22:08 +0530

    added project

---
+++ src/main/java/com/maildemo/mailapp/data/jpa/services/JavaSmtpGmailSenderService.java
@@ -0,0 +1,25 @@
+package com.maildemo.mailapp.data.jpa.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JavaSmtpGmailSenderService {
+	
+	@Autowired
+    private JavaMailSender emailSender;
+
+    public void sendEmail(String toEmail, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("botrate@gmail.com");
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+
+        emailSender.send(message);
+
+        System.out.println("Message sent successfully");
+    }
+}
